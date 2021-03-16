02bd7ca5d6e3555c81129fff0e1da851805a1142
Synth <synth9283@gmail.com>
2021-03-16 15:00:47 -0400

    Unstable port

--- main.c
+++ main.c
@@ -9,19 +9,18 @@ int main(int argc, char **argv) {
     builder = gtk_builder_new_from_file("glade/main.glade");
     yttWidget.windowMain = GTK_WIDGET(gtk_builder_get_object(builder, "windowMain"));
     yttWidget.entryUrl = GTK_WIDGET(gtk_builder_get_object(builder, "entryUrl"));
-    yttWidget.buttonSave = GTK_WIDGET(gtk_builder_get_object(builder, "buttonSave"));
+    // yttWidget.buttonSave = GTK_WIDGET(gtk_builder_get_object(builder, "buttonSave"));
     yttWidget.progressDownload = GTK_WIDGET(gtk_builder_get_object(builder, "progressDownload"));
     yttWidget.imageVideo = GTK_WIDGET(gtk_builder_get_object(builder, "imageVideo"));
 
-    // g_signal_connect(builder, "activate", G_CALLBACK(activate), NULL);
     g_object_unref(builder);
     gtk_widget_show(yttWidget.windowMain);
-    // gtk_main();
+    g_main_run(yttWidget.windowMain);
 
     return 0;
 }
 
 // void on_windowMain_destroy()
 // {
-//     gtk_main_quit();
+//     g_main_quit(NULL);
 // }
